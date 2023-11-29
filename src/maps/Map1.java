a55966cfc9a8f7c7654e0ce694c90bbf47455a3a
Pablo Henrique <105394456+Pablo-henrique23@users.noreply.github.com>
2023-11-29 04:13:26 +0000

    Add files via upload
    classe Button aperfeiçoada, criados botoes de `jogar`, `quit` e `menu`

--- src/maps/Map1.java
+++ src/maps/Map1.java
@@ -1,12 +1,14 @@
 package src.maps;
 
 import src.Janela;
+import src.Scene;
+import src.behaviors.ClickableArea;
 import src.behaviors.Collision;
 import src.behaviors.SpawnGameObject;
 import src.components.GameObject;
 import src.components.PhysicsObject;
 import src.components.Player;
-
+import src.components.Button;
 
 public class Map1 extends Maps {
     public Map1() {
@@ -21,7 +23,14 @@ public class Map1 extends Maps {
         //* spawner.objectToSpawn = new AlgumTipoDeObjeto(args...);
         Player player = new Player(objectsListBuffer);
         objectsList.add(player);
-
+        // Nota: Janela.HEIGHT/WIDTH nos da metade do tamanho da tela, por isso esta multiplicado por 
+        // 2 logo abaixo
         objectsList.add(spawner);
+        Button menu = new Button("Menu", Janela.WIDTH*2 - 200, Janela.HEIGHT*2-100, 150, 50);
+        menu.behaviors.add(new ClickableArea(menu, () -> {
+            System.out.println("Back to Menu");
+            Scene.selectedMapId = 0;
+        }));
+        objectsList.add(menu);
     }
 }
